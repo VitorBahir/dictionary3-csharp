f2acf9e8a54e4218a513b9eae5181a068a3b9753
Vitor Bahir <vitorb82014@gmail.com>
2023-03-29 20:50:59 -0300

    Initial implementation

---
+++ Course/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public Product(int id, int quantity, double price)
+        {
+            Id = id;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double Total(int quantity, double price)
+        {
+            return Price * quantity;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>();
+        Dictionary<int, int> Items = new Dictionary<int, int>();
+
+        Console.WriteLine("Digite o numero do produto e a quantidade desejada: (número, quantidade) ");
+        Console.WriteLine(" (1 - Hot dog R$ 1,00) ");
+        Console.WriteLine(" (2 - Hamburguer R$ 1,30) ");
+        Console.WriteLine(" (3 - Cheeseburguer R$ 1,50) ");
+        Console.WriteLine(" (4 - Refrigerante em lata R$ 1,00) ");
+        Console.WriteLine(" (5 - Batatas fritas R$ 0,80) ");
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            string[] pedido = Console.ReadLine().Split(" ");
+            int numberOrder = int.Parse(pedido[0]);
+            int qttOrder = int.Parse(pedido[1]);
+            Items.Add(numberOrder, qttOrder);
+        }
+
+        foreach (var item in Items)
+        {
+            if (item.Key == 1)
+            {
+                products.Add(new Product(item.Key, item.Value, 1.0));
+                if (!(item.Value == 0))
+                {
+                    Console.WriteLine("Quantidade de hotdog: " + item.Value);
+                }
+
+            }
+            else if (item.Key == 2)
+            {
+                products.Add(new Product(item.Key, item.Value, 1.3));
+                if (!(item.Value == 0))
+                {
+                    Console.WriteLine("Quantidade de hamburguer: " + item.Value);
+                }
+
+
+            }
+            else if (item.Key == 3)
+            {
+                products.Add(new Product(item.Key, item.Value, 1.5));
+                if (!(item.Value == 0))
+                {
+                    Console.WriteLine("Quantidade de cheeseburguer: " + item.Value);
+
+                }
+            }
+            else if (item.Key == 4)
+            {
+                products.Add(new Product(item.Key, item.Value, 1.0));
+                if (!(item.Value == 0))
+                {
+                    Console.WriteLine("Quantidade de refri: " + item.Value);
+                }
+
+            }
+            else if (item.Key == 5)
+            {
+                products.Add(new Product(item.Key, item.Value, 0.8));
+                if (!(item.Value == 0))
+                {
+                    Console.WriteLine("Quantidade de batatas fritas: " + item.Value);
+                }
+            }
+        }
+        Console.WriteLine();
+
+        double soma = 0;
+        foreach (Product product in products)
+        {
+            soma += product.Total(product.Quantity, product.Price);
+        }
+
+        Console.WriteLine("Valor total do pedido: " + soma.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
