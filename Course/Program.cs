4f8d2eb5ad766e07d4bbd61827e68c2a7ea73216
Vitor Bahir <vitorb82014@gmail.com>
2023-03-29 22:26:54 -0300

    Implementation without repeated code

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using Course.Entities;
 
@@ -8,80 +9,60 @@ public class Program
     public static void Main(string[] args)
     {
         List<Product> products = new List<Product>();
-        Dictionary<int, int> Items = new Dictionary<int, int>();
+        Dictionary<int, int> orders = new Dictionary<int, int>();
+        Dictionary<int, double> items = new Dictionary<int, double>()
+        {
+            {1, 1.00 }, {2, 1.30}, {3, 1.50}, {4, 1.0}, {5, 0.8}
+        };
 
-        Console.WriteLine("Digite o numero do produto e a quantidade desejada: (número, quantidade) ");
-        Console.WriteLine(" (1 - Hot dog R$ 1,00) ");
-        Console.WriteLine(" (2 - Hamburguer R$ 1,30) ");
-        Console.WriteLine(" (3 - Cheeseburguer R$ 1,50) ");
-        Console.WriteLine(" (4 - Refrigerante em lata R$ 1,00) ");
-        Console.WriteLine(" (5 - Batatas fritas R$ 0,80) ");
+        bool status = true;
+        Console.WriteLine(" (1 - Hot dog U$ 1.00) ");
+        Console.WriteLine(" (2 - Hamburguer U$ 1.30) ");
+        Console.WriteLine(" (3 - Cheeseburguer U$ 1.50) ");
+        Console.WriteLine(" (4 - Cans U$ 1.00) ");
+        Console.WriteLine(" (5 - French fries U$ 0.80) ");
+        Console.WriteLine(" Do want go out of screen, type 0");
         Console.WriteLine();
 
-        for (int i = 0; i < 5; i++)
+        while (status)
         {
-            string[] pedido = Console.ReadLine().Split(" ");
-            int numberOrder = int.Parse(pedido[0]);
-            int qttOrder = int.Parse(pedido[1]);
-            Items.Add(numberOrder, qttOrder);
+            Console.WriteLine("Enter product number first, then the desired quantity:");
+            int numberOrder = int.Parse(Console.ReadLine());
+            if (numberOrder != 0)
+            {
+                Console.WriteLine("Type the order's quantity: ");
+                int qttOrder = int.Parse(Console.ReadLine());
+
+                if (orders.ContainsKey(numberOrder))
+                {
+                    orders[numberOrder] += qttOrder;
+                }
+                else
+                {
+                    orders[numberOrder] = qttOrder;
+                }
+            }
+            else
+            {
+                status = false;
+            }
         }
 
-        foreach (var item in Items)
+        foreach (var order in orders)
         {
-            if (item.Key == 1)
-            {
-                products.Add(new Product(item.Key, item.Value, 1.0));
-                if (!(item.Value == 0))
-                {
-                    Console.WriteLine("Quantidade de hotdog: " + item.Value);
-                }
-
-            }
-            else if (item.Key == 2)
-            {
-                products.Add(new Product(item.Key, item.Value, 1.3));
-                if (!(item.Value == 0))
-                {
-                    Console.WriteLine("Quantidade de hamburguer: " + item.Value);
-                }
-
-
-            }
-            else if (item.Key == 3)
-            {
-                products.Add(new Product(item.Key, item.Value, 1.5));
-                if (!(item.Value == 0))
-                {
-                    Console.WriteLine("Quantidade de cheeseburguer: " + item.Value);
-
-                }
-            }
-            else if (item.Key == 4)
-            {
-                products.Add(new Product(item.Key, item.Value, 1.0));
-                if (!(item.Value == 0))
-                {
-                    Console.WriteLine("Quantidade de refri: " + item.Value);
-                }
-
-            }
-            else if (item.Key == 5)
-            {
-                products.Add(new Product(item.Key, item.Value, 0.8));
-                if (!(item.Value == 0))
-                {
-                    Console.WriteLine("Quantidade de batatas fritas: " + item.Value);
-                }
-            }
+            Console.WriteLine($"Item price {order.Key}: " + items[order.Key]);
+            
+            products.Add(new Product(order.Key, order.Value, items[order.Key]));
         }
+
         Console.WriteLine();
 
-        double soma = 0;
+        double sum = 0;
         foreach (Product product in products)
         {
-            soma += product.Total(product.Quantity, product.Price);
+            sum += product.Total(product.Quantity, product.Price);
         }
 
-        Console.WriteLine("Valor total do pedido: " + soma.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Order total value: " + sum.ToString("F2", CultureInfo.InvariantCulture));
     }
 }
